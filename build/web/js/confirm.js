19a86aba7e0e33fa4d391284e654f3f6cee70e48
Đạt Nguyễn <dat19991203@gmail.com>
2022-03-22 22:06:24 +0700

    Update

---
+++ build/web/js/confirm.js
@@ -0,0 +1,12 @@
+function confirmFunction() {
+    if (confirm("Bạn có chắc chắn với những thay đổi này?") == true) {
+        alert("Lưu thành công!");
+        location.reload();
+    }
+}
+function confirmChangePass() {
+    if (confirm("Bạn có chắc chắn với việc thay đổi mật khẩu này?") == true) {
+        alert("Lưu thành công!");
+        location.reload();
+    }
+}
