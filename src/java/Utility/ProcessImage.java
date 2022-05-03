da7130a38628e590fc31e15dd55f1a91fa828df6
Đạt Nguyễn <dat19991203@gmail.com>
2022-05-04 00:34:28 +0700

    update process image

--- src/java/Utility/ProcessImage.java
+++ src/java/Utility/ProcessImage.java
@@ -5,11 +5,37 @@
  */
 package Utility;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import org.apache.commons.fileupload.FileItem;
 
 /**
  *
  * @author dat19
  */
 public class ProcessImage {
-    
+    public final static String USER_IMAGE_PATH = "/Assets/images/avatar";
+    public final static String PRODUCT_IMAGE_PATH = "/Assets/images/product";
+
+    public String storeUserImage(FileItem item, ServletContext servletContext, String username) {
+        try {
+            String storePath = servletContext.getRealPath(USER_IMAGE_PATH);
+            File uploadFile = new File(storePath + File.separator
+                    + "avatar_image_" + username
+                    + item.getContentType().replaceFirst("image/", "."));
+            if (uploadFile.exists()) {
+                uploadFile.delete();
+            }
+            item.write(uploadFile);
+            String imagePath = USER_IMAGE_PATH
+                    + "avatar_image_" + username
+                    + item.getContentType().replaceFirst("image/", ".");
+            return imagePath;
+        } catch (Exception ex) {
+            Logger.getLogger(ProcessImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
